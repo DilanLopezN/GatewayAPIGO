71bac029f71820bc38a15e690f3fcb9452863805
Dilan lopez <dilanlopez009@gmail.com>
2025-04-10 00:50:42 -0300

    feat: finish entities

--- internals/http/entities/invoice.go
+++ internals/http/entities/invoice.go
@@ -22,13 +22,44 @@ type CreateInvoiceInput struct {
 }
 
 type InvoiceOutput struct {
-	ID 					   string  
-	AccountID 		 string   
-	Status         string   
-	Amount         float64  
-	Description    string   
-  CardLastDigits string   
-	CreatedAt      string   
-	UpdatedAt      string   
-
+	ID 						 string   `json:"id"`  
+	AccountID 		 string   `json:"account_id"`
+	Status         string   `json:"status"`
+	Amount         float64  `json:"amount"`
+	Description    string   `json:"description"`
+	PaymentType    string   `json:"payment_type"`
+  CardLastDigits string   `json:"card_last_digits"`
+	CreatedAt      string   `json:"created_at"`
+	UpdatedAt      string   `json:"updated_at"`
 }
+
+func ToInvoice(input *CreateInvoiceInput, accountId string) (*domain.Invoice, error) {
+	creditCard := domain.CreditCard{
+		Number:        input.CardNumber,
+		CVV:           input.CCV,
+		ExpiryMonth:   input.ExpiryMonth,
+		ExpiryYear:    input.ExpiryYear,
+		CardHolderName: input.CardHolderName,
+	}
+
+	invoice, err := domain.NewInvoice(accountId, input.Amount, input.Description, input.PaymentType, creditCard)
+	if err != nil {
+		return nil, err
+	}
+
+	return invoice, nil
+}
+
+func FromInvoice(invoice *domain.Invoice) InvoiceOutput {
+	return InvoiceOutput{
+		ID:             invoice.ID,
+		AccountID:      invoice.AccountID,
+		Status:         string(invoice.Status),
+		Amount:         invoice.Amount,
+		Description:    invoice.Description,
+		PaymentType:    invoice.PaymentType,
+		CardLastDigits: invoice.CardLastDigits,
+		CreatedAt:      invoice.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt:      invoice.UpdatedAt.Format("2006-01-02 15:04:05"),
+	}
+}	
